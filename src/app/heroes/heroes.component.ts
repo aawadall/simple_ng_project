46cf6a5ad1653b8f1305801ac0902f7b0a898652
Ahmed Awadallah <aawadall@ualberta.ca>
2022-12-17 15:38:22 -0500

    add a service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from "../hero";
-import { HEROES } from "../mock-heroes";
+import { HeroService } from "../hero.service";   
 // Decorator function containing component metadata 
 @Component({
   selector: 'app-heroes', // component css element selector 
@@ -11,17 +11,20 @@ import { HEROES } from "../mock-heroes";
 
 // note we export the component 
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
+  heroes: Hero[] = [];
   selectedHero?: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
-  constructor() {}
+  constructor(private heroService: HeroService) {}
 
   // lifecycle hook, this is called shortly after creating the component 
   // good for initialization code 
   ngOnInit(): void { 
- 
+    this.getHeroes();
   }
 
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
 }
